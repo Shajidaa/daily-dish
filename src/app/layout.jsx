2f76c2760033fe52fb50fe89ff53fecbb841c9bd
Shajidaa <shajidaakterlopa101@gmail.com>
2025-12-16 23:04:32 +0600

    font optimatize

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+
 import Navbar from "@/components/Navbar/Navbar";
 import CartProvider from "@/context/CartProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  weight: ["400", "500", "600"],
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: {
     default: "Daily Dish",
@@ -25,9 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
-      >
+      <body className={`${poppins.className} antialiased `}>
         <CartProvider>
           <Navbar></Navbar>
           {children}
